c04a6cb166d9b9287f4ad321b3a477f3347c7db7
Alex Gheorghita <gheorghitacristian@mac.com>
2018-03-20 00:19:17 +0200

    Added check account balance command

---
+++ src/Eurid/Frames/CheckBalance.php
@@ -0,0 +1,41 @@
+<?php
+namespace AgileGeeks\EPP\Eurid\Frames;
+
+use AgileGeeks\EPP\Eurid\Frames\Command;
+
+require_once(__DIR__ . '/Command.php');
+
+class CheckBalance extends Command
+{
+    const TEMPLATE = <<<XML
+    <command>
+        <info>
+            <registrar:info xmlns:registrar="http://www.eurid.eu/xml/epp/registrarFinance-1.0"/>
+        </info>
+        <clTRID>%s</clTRID>
+  </command>
+XML;
+
+    function __construct()
+    {
+        $this->xml = sprintf(
+            self::TEMPLATE,
+            $this->clTRID()
+        );
+    }
+
+    function getResult($dom)
+    {
+        parent::getResult($dom);
+
+        $resData_node = $dom->getElementsByTagName('resData')->item(0);
+        $infData_node = $resData_node->getElementsByTagName('infData')->item(0);
+        
+        $result = new \stdClass();
+        $result->paymentMode = $infData_node->getElementsByTagName('paymentMode')->item(0);
+        $result->availableAmount = $infData_node->getElementsByTagName('availableAmount')->item(0);
+        $result->accountBalance = $infData_node->getElementsByTagName('paymentMode')->item(0);
+
+        return $result;
+    }
+}
